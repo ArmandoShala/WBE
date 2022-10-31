3ca37e358d8b2268f9192dd8362067d776f1c71b
Armando <shalaar3@students.zhaw.ch>
2022-10-31 22:19:21 +0100

    WBE8 done

--- Labs/Lab7/currentTemp.js
+++ Labs/Lab7/currentTemp.js
@@ -1,22 +1,21 @@
-/*Aufgabe 2: REST-Zugriff aus einem Script (Abgabe)
-Schreiben Sie ein Node.js-Script currentTemp.js, welches wttr.in verwendet, um die aktuelle Temperatur an einem bestimmten Ort auszugeben:1
-$ node currentTemp.js Winterthur
-11°
-$ node currentTemp.js Madrid
-15°
-Verwenden Sie https.get des https-Moduls von Node.js. Die Daten des eingehenden Streams können
-Sie einsammeln, indem Sie data-Events bearbeiten. Das end-Event zeigt den Abschluss des Streams
-an, so dass die Daten mit JSON.parse ausgewertet werden können.*/
+ const https = require('node:https');
 
-function currentTemp(location) {
-    const https = require('https');
-    const url = `https://wttr.in/${location}?format=j1`;
-    https.get(url, res => {
+function logWeatherIn() {
+    const name = process.argv[2] || 'Zurich';
+    const URL = 'https://wttr.in/' + name + '?format=j1';
+    https.get(URL, (res) => {
         let data = '';
+        if (res.statusCode !== 200) {
+            throw new Error('Request failed');
+        }
         res.on('data', chunk => data += chunk);
         res.on('end', () => {
             const weather = JSON.parse(data);
-            console.log(weather.current_condition[0].temp_C);
+            console.log(`${weather.current_condition[0].FeelsLikeC}°C`);
         });
+    }).on('error', (e) => {
+        console.error(e);
     });
 }
+
+logWeatherIn();
