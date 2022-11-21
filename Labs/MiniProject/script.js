ab8b2b846491b12d82a7893ba20ddd35f87e3342
Armando <dreiteilig.backoefen_0f@icloud.com>
2022-11-21 20:15:13 +0100

    P8 A1 done

--- Labs/MiniProject/script.js
+++ Labs/MiniProject/script.js
@@ -1,4 +1,4 @@
-function elt (type, attrs, ...children) {
+function elt(type, attrs, ...children) {
     let node = document.createElement(type)
     Object.keys(attrs).forEach(key => {
         node.setAttribute(key, attrs[key])
@@ -10,5 +10,34 @@ function elt (type, attrs, ...children) {
     return node
 }
 
-document.getElementsByClassName("board")[0].appendChild(elt("div", {id: "container"}, "Hello World!"))
-console.log("Hello World!")
+function initGameBoard(rows = 6, cols = 7) {
+    let board = elt("div", {class: "board"})
+    for (i = 1; i <= rows; i++) {
+        let row = elt("div", {class: "row"})
+        for (j = 1; j <= cols; j++) {
+            let field = elt("div", {class: "field"})
+            row.appendChild(field)
+        }
+        board.appendChild(row)
+    }
+    return board
+}
+
+function addGamePiecesAtRandomPosition(color) {
+    let piece = elt("div", {class: color + " piece"})
+    let board = document.getElementsByClassName("board")[0]
+    let row = board.children[Math.floor(Math.random() * board.children.length)]
+    let field = row.children[Math.floor(Math.random() * row.children.length)]
+    if (field.children.length !== 0) {
+        addGamePiecesAtRandomPosition(color)
+    }
+    field.appendChild(piece)
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    document.body.appendChild(initGameBoard())
+    window.localStorage.setItem("coord1", "tbd")
+    addGamePiecesAtRandomPosition("red")
+    addGamePiecesAtRandomPosition("blue")
+});
+
