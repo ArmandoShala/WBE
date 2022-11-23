2f8558568c05b9bffacd5cc3e5bd9c96ff84ca5a
Armando <dreiteilig.backoefen_0f@icloud.com>
2022-11-23 21:57:28 +0100

    Been there done that

---
+++ Labs/Playground/main.js
@@ -0,0 +1,8 @@
+const {request} = require("http")
+let rq = request({hostname: "localhost", port: 8000, method: "POST"}, response => {
+    response.on("data", chunk => process.stdout.write(chunk.toString()));
+})
+
+rq.write("Hello server\n")
+rq.write("And good bye\n")
+rq.end()
